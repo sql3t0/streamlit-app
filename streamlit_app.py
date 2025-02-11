cede545a11238ee36cd56d89f588ac1a64ffce00
sql3t0 <dj.sqleto@gmail.com>
2025-02-11 14:11:16 +0000

    teste http.server

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
 import streamlit as st
-
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write(
-    "![teste](file:///etc/passwd)"
-)
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
+
+# filec = open('/etc/passwd').read()
+# #print(filec)
+
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+# st.write(
+#     f"{filec}"
+# )
